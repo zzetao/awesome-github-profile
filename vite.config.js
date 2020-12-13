d11a8ed988dac40490b169df98a84f21d8ed72da
zzetao <izzetao@gmail.com>
2020-12-13 17:24:47 +0800

    fix: favicon href is undefined after build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+// https://github.com/vitejs/vite/pull/1218
+function fixFaviconBugPlugin() {
+    return {
+        rollupInputOptions: {
+            pluginsPostBuild: [
+                {
+                    name: 'fix-favicon-bug-plugin',
+                    async writeBundle() {
+                        const htmlPath = path.resolve('./dist/index.html')
+                        let html = fs.readFileSync(htmlPath).toString()
+                        html = html.replace('href="undefined"', 'href=/assets/awesome.ico')
+
+                        fs.writeFileSync(htmlPath, html)
+                    }
+                }
+            ]
+        }
+    }
+}
+
 module.exports = {
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    plugins: [
+        fixFaviconBugPlugin()
+    ]
 }
