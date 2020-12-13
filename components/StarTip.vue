52c87168e29732dfdfaa153c57a9079981957232
abhisheknaiidu <geekyabhi12@gmail.com>
2020-12-13 14:07:13 +0530

    corrected links and favicon

--- components/StarTip.vue
+++ components/StarTip.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="github-link" href="https://github.com/zzetao/awesome-github-profile" onclick="window.open('https://github.com/abhisheknaiidu/awesome-github-profile-readme');" >GitHub</a>
+    <a class="github-link" href="https://github.com/abhisheknaiidu/awesome-github-profile-readme" >Add Your Profile</a>
 </template>
 
 <script>
